cccdd613a17d3bc12a33e8eaa1428a12d1683a6d
ArminRodriguez1 <44068727+ArminRodriguez1@users.noreply.github.com>
2023-12-05 01:36:12 -0300

    update
    update

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -6,4 +6,18 @@ describe("Home", () => {
 			"http://pruebas-soft.s3-website.us-east-2.amazonaws.com/login"
 		);
 	});
+	it("[Successful H-2] Home correct", () => {
+		cy.login().then(({ token, user }) => {
+		  expect(token).to.exist;
+		  expect(user).to.exist;
+		  expect(user.name).to.exist;
+	  
+		  cy.visit("/", {
+			failOnStatusCode: false,
+		  });
+	  
+		  cy.get('h2').should('contain', `Welcome ${user.name},`);
+		});
+	  });
+	  
 });
